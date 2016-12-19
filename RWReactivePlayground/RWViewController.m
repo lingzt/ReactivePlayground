4050922c28502a062b258b93f74a47c037cd58da
Lingzt <asklingtoby@gmail.com>
2016-12-19 00:09:24 -0500

    add simple RACSignl and subscript next

---
+++ RWReactivePlayground/RWViewController.m
@@ -0,0 +1,66 @@
+//
+//  RWViewController.m
+//  RWReactivePlayground
+//
+//  Created by Colin Eberhardt on 18/12/2013.
+//  Copyright (c) 2013 Colin Eberhardt. All rights reserved.
+//
+
+#import "RWViewController.h"
+#import "RWDummySignInService.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+@interface RWViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *signInButton;
+@property (weak, nonatomic) IBOutlet UILabel *signInFailureText;
+
+@property (nonatomic) BOOL passwordIsValid;
+@property (nonatomic) BOOL usernameIsValid;
+@property (strong, nonatomic) RWDummySignInService *signInService;
+
+@end
+
+@implementation RWViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  self.signInService = [RWDummySignInService new];
+  RACSignal *textSignal = [self.usernameTextField rac_textSignal];
+  [textSignal subscribeNext:^(id x) {
+      NSLog(@"%@",x);
+  }];
+
+}
+
+- (BOOL)isValidUsername:(NSString *)username {
+  return username.length > 3;
+}
+
+- (BOOL)isValidPassword:(NSString *)password {
+  return password.length > 3;
+}
+
+- (IBAction)signInButtonTouched:(id)sender {
+  // disable all UI controls
+  self.signInButton.enabled = NO;
+  self.signInFailureText.hidden = YES;
+
+  // sign in
+  [self.signInService signInWithUsername:self.usernameTextField.text
+                            password:self.passwordTextField.text
+                            complete:^(BOOL success) {
+                              self.signInButton.enabled = YES;
+                              self.signInFailureText.hidden = success;
+                              if (success) {
+                                [self performSegueWithIdentifier:@"signInSuccess" sender:self];
+                              }
+                            }];
+}
+
+
+
+
+@end
